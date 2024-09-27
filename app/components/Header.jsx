81a09210a5494d096651858b47203ab6cd8a8e9e
Anni <annika.schatter@web.de>
2024-09-27 20:03:10 +0200

    markers and popups working

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,9 @@
 export default function Header() {
-    return (
-        <header className="header">
-          <h2 className="header">
-            Willkommen zu dieser
-          </h2>
-          <h1 className="header">webbasierten Kartenanwendung</h1>
-        </header>
-    )
+  console.log("hello World");
+  return (
+    <header className="header">
+      <h2 className="header">Willkommen zu dieser</h2>
+      <h1 className="header">webbasierten Kartenanwendung</h1>
+    </header>
+  );
 }
--- app/components/map/Map.jsx
+++ app/components/map/Map.jsx
@@ -1,10 +1,15 @@
 "use client";
 
 import "leaflet/dist/leaflet.css";
-
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+import MapClickHandler from "./MapClickHandler";
 
 export default function Map() {
+  console.log("hello world");
+
+  const [marker, setMarker] = useState([]);
+
   return (
     <MapContainer
       center={[51.0, 9.0]}
@@ -12,10 +17,23 @@ export default function Map() {
       scrollWheelZoom={true}
       className="map-container"
     >
+      <MapClickHandler
+        clickHandler={(latlng) => {
+          setMarker(marker.concat([latlng]));
+        }}
+      />
       <TileLayer
         attribution='&copy; <a href:"https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {marker.map((m, index) => (
+        <Marker position={m} key={index}>
+          <Popup>
+            <p>Latitude: {m.lat}</p>
+            <p>Longitude: {m.lng}</p>
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
